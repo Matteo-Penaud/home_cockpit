a3f84b35fa067482f119fabfca748e5788412088
Matteo-Penaud <Matteo-Penaud@users.noreply.github.com>
2025-07-28 21:09:09 +0200

    encode frame function in protocol

--- pc/protocol/packet.py
+++ pc/protocol/packet.py
@@ -1,3 +1,7 @@
+START_BYTE = 0xAA
+STOP_BYTE = 0x55
+
+
 def compute_checksum(data: bytes) -> int:
     try:
         result = 0
@@ -5,5 +9,12 @@ def compute_checksum(data: bytes) -> int:
             result ^= b
     except TypeError:
         raise AssertionError("Input data should be bytes")
-    
+
     return result
+
+
+def encode(req_id: int, data: bytes) -> bytes:
+    frame = [START_BYTE, req_id, len(data)] + [x for x in data] + [0x00, STOP_BYTE]
+    frame[-2] = compute_checksum(frame)
+
+    return bytes(frame)
--- pc/tests/test_packet.py
+++ pc/tests/test_packet.py
@@ -1,5 +1,6 @@
-from protocol.packet import compute_checksum
+from protocol.packet import compute_checksum, START_BYTE, STOP_BYTE, encode
 import pytest
+import random
 
 
 def test_compute_checksum_single_byte():
@@ -25,3 +26,30 @@ def test_compute_checksum_invalid_value():
 
     with pytest.raises(AssertionError):
         compute_checksum(85)
+
+
+def test_encode_single_byte():
+    expected_frame_raw = [START_BYTE, 0x00, 0x01, 0xBB, 0x00, STOP_BYTE]
+    expected_frame_raw[-2] = compute_checksum(bytes(expected_frame_raw))
+
+    assert encode(0, bytes([0xBB])) == bytes(expected_frame_raw)
+
+
+def test_encode_multiple_bytes():
+    random_data = [b for b in random.randbytes(0x10)]
+    expected_frame_raw = [START_BYTE, 0xCA, 0x10] + random_data + [0x00, STOP_BYTE]
+    expected_frame_raw[-2] = compute_checksum(bytes(expected_frame_raw))
+
+    assert encode(0xCA, bytes(random_data)) == bytes(expected_frame_raw)
+
+
+def test_encode_empty_data():
+    expected_frame_raw = [START_BYTE, 0xFE, 0x00, 0x00, STOP_BYTE]
+    expected_frame_raw[-2] = compute_checksum(bytes(expected_frame_raw))
+
+    assert encode(0xFE, bytes()) == bytes(expected_frame_raw)
+
+
+def test_encode_invalid_req_id():
+    with pytest.raises(ValueError):
+        encode(0x100, bytes())
